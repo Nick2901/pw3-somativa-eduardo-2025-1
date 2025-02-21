60ef73a1bbb80bd02fef6197e02b0710474ba0fd
Nick2901 <dudasbarbosa2@gmail.com>
2025-02-20 22:34:09 -0300

    Começando a trabalhar a NavBar.

--- pw3-lucaProjeto/src/App.jsx
+++ pw3-lucaProjeto/src/App.jsx
@@ -1,29 +1,11 @@
 import './App.css'
-import BookCard from './components/BookCard'
-import capa_livro from './assets/cavernas_aco.jpg'
+import NavBar from './components/layout/NavBar'
 
 function App() {
-
   return (
     <>
       <div>
-        <h1>PW3 - WEB APP - LIVRARIA</h1>
-        <BookCard 
-          titulo = 'As Cavernas de Aço' 
-          autor = 'Isaac Azimov'
-          imagem = {capa_livro}/>
-        {/* <BookCard 
-          titulo = 'O Sol Desvelado' 
-          autor = 'Isaac Azimov'
-          imagem = 'Teste'/>
-        <BookCard 
-          titulo = 'O Fim da Infância' 
-          autor = 'Arthur C. Clark'
-          imagem = 'Teste'/>
-        <BookCard 
-          titulo = 'Neuromancer' 
-          autor = 'Willian Gibson'
-          imagem = 'Teste'/> */}
+        <NavBar/>
       </div>
     </>
   )
---
+++ pw3-lucaProjeto/src/components/layout/NavBar.jsx
@@ -0,0 +1,15 @@
+import style from './NavBar.modules.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>CADASTRAR LIVROS</li>
+                <li>LISTAR LIVROS</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
