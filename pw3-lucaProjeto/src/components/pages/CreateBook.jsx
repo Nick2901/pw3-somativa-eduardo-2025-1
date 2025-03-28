c2bf39a01aa75742ca83897bf36320129d43f9b1
Nick2901 <dudasbarbosa2@gmail.com>
2025-03-27 22:29:22 -0300

    Conexão com apirest

--- pw3-lucaProjeto/src/components/pages/CreateBook.jsx
+++ pw3-lucaProjeto/src/components/pages/CreateBook.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import style from './CreateBook.module.css'
 import Input from "../form/Input";
 import Select from "../form/Select";
@@ -9,19 +9,41 @@ const CreateBook = ()=>{
 
     const [book, setBook] = useState({});
 
+    const [categories, setCategories] = useState([]);
+
+
     function handlerChangeBook(event){
         setBook({...book, [event.target.name] : event.target.value});
         console.log(book);
     }
 
     function handlerChangeCategory(event){
-        setBook({...book, category : event.target.options[event.target.selectedIndex].text})
+        setBook({...book, cod_categoria : event.target.options[event.target.selectedIndex].text})
     }
 
     function submit(event){
         event.preventDefault();
         console.log(book);
     }
+
+useEffect(() => {
+    fetch('http://127.0.0.1:5000/listagemCateorias',
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Headers':'*'
+            }
+        }
+    ).then((resp) =>
+        resp.json()
+    ).then((categorias) => {
+        console.log('TESTE: ' + categorias.data);
+    }).catch((error) => {
+        console.log('ERRO: ' + error);
+    })
+}, []);
 
     return(
         <section className = {style.create_book_container}>
@@ -31,26 +53,26 @@ const CreateBook = ()=>{
                     handlerChange = {handlerChangeBook}
                     text = 'Nome do Livro' 
                     type = 'text'
-                    name = 'txt_livro' 
+                    name = 'nome_livro' 
                     id = 'txt_livro' 
                     placeholder = 'Digite o nome do livro'/>
                 <Input 
                     handlerChange = {handlerChangeBook}
                     text = 'Nome do Autor' 
                     type = 'text'
-                    name = 'txt_autor' 
+                    name = 'autor_livro' 
                     id = 'txt_autor' 
                     placeholder = 'Digite o nome do autor do livro'/>
                 <Input 
                     handlerChange = {handlerChangeBook}
                     text = 'Descrição do Livro' 
                     type = 'text'
-                    name = 'txt_descricao' 
+                    name = 'descricao_livro' 
                     id = 'txt_descricao' 
                     placeholder = 'Digite a descrição do livro'/>
                 <Select
                     handlerChange = {handlerChangeCategory}
-                    name = 'slc_categoria'
+                    name = 'cod_categoria'
                     id = 'slc_categoria'
                     text = 'Categoria do livro'/>
                 <Button
